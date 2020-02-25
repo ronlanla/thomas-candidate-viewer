cfebf8ae1155aa97a51a4db728053d424f6badc1
ronlanla <myron836@hotmail.co.uk>
2020-02-25 22:11:07 +0000

    Update candidate-view types

--- src/types/components/candidate-view/candidate-view.ts
+++ src/types/components/candidate-view/candidate-view.ts
@@ -1,14 +1,34 @@
+export type CandidateAssessments = {
+  // Personal Profile Analysis
+  ppa?: {
+    sent_dt?: string,
+    complete_dt?: string,
+    result?: string
+  }
+  // General Intelligence Assessment
+  gia?: {
+    sent_dt?: string,
+    complete_dt?: string,
+    result?: string
+  }
+  // Trait Emotional Intelligence Questionnaire
+  teique?: {
+    sent_dt?: string,
+    complete_dt?: string,
+    result?: string
+  }
+  // High Potential Trait Indicator
+  hpti?: {
+    sent_dt?: string,
+    complete_dt?: string,
+    result?: string
+  }
+}
+
 export type CandidateData = {
   id: number,
   name: string,
-  assessments: {
-    // Personal Profile Analysis
-    ppa: {
-      sent_dt?: string,
-      complete_dt?: string,
-      result?: string
-    }
-  }
+  assessments: CandidateAssessments;
 }
 
 export type CandidateDataList = CandidateData[]
