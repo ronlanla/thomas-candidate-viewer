1318b66ed3ada483420456deb08df6ac0b639e8a
ronlanla <myron836@hotmail.co.uk>
2020-02-25 21:21:23 +0000

    Add thomas font to app

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 import 'normalize.css';
+import './resources/fonts/thomas-font/style.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
