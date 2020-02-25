aedd9b075e0cd68bde2b0fa952d47622974d7768
ronlanla <myron836@hotmail.co.uk>
2020-02-25 21:28:06 +0000

    Add global icon component

---
+++ src/components/global/icon/index.tsx
@@ -0,0 +1,19 @@
+import classnames from 'classnames';
+import React, { Component } from 'react';
+
+import './icon.css';
+
+interface IconProps {
+  name: string;
+  class?: string;
+}
+
+class Icon extends Component<IconProps> {
+  render () {
+    return (
+      <div className={classnames(`icon-${this.props.name}`, this.props.class)}/>
+    );
+  }
+}
+
+export default Icon;
